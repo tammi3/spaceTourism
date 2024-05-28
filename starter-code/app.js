3aea437b3486057c5fe68a2886dfb11698385c60
tammi3 <precioushobobo@gmail.com>
2024-05-28 16:38:03 +0100

    made the home page responsive

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,24 +1,21 @@
 new Vue({
-    el:'#vue-app',
-    data:{
-        burger:'',
-        menu:'',
-        logo:''
+  el: "#vue-app",
+  data: {
+    burger: "",
+    menu: "",
+  },
+  methods: {
+    toggleMenu() {
+      this.burger = document.querySelector("#burger");
+      this.menu = document.querySelector("#menu");
+
+      if (this.menu.classList.contains("hidden")) {
+        this.menu.classList.remove("hidden");
+        this.burger.classList.add("hidden");
+      } else {
+        this.menu.classList.add("hidden");
+        this.burger.classList.remove("hidden");
+      }
     },
-    methods:{
-        toggleMenu(){
-            this.burger = document.querySelector('#burger');
-             this.menu = document.querySelector('#menu');
-             this.logo = document.querySelector('#logo');
-             if (this.menu.classList.contains('hidden')) {
-                this.menu.classList.remove('hidden');
-                this.burger.classList.add('hidden');
-                this.logo.classList.add('hidden');
-              } else {
-                this.menu.classList.add('hidden');
-                this.burger.classList.remove('hidden');
-                this.logo.classList.remove('hidden');
-              }
-        }
-    }
-})
+  },
+});
