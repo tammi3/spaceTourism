135691e6c9fbfef698e6c0e3a498888519f98b9b
tammi3 <precioushobobo@gmail.com>
2024-05-30 16:47:35 +0100

    i have fixed the bugs, adding some finishing touch to the navbar because of an adjustment i made.

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,17 +3,21 @@ new Vue({
   data: {
     burger: "",
     menu: "",
+    navBar:""
   },
   methods: {
     toggleMenu() {
       this.burger = document.querySelector("#burger");
       this.menu = document.querySelector("#menu");
+      this.navBar = document.querySelector("#nav-bar");
 
       if (this.menu.classList.contains("hidden")) {
         this.menu.classList.remove("hidden");
+        this.navBar.classList.remove("items-center");
         this.burger.classList.add("hidden");
       } else {
         this.menu.classList.add("hidden");
+        this.navBar.classList.add("items-center");
         this.burger.classList.remove("hidden");
       }
     },
